f1d05fec5cfad8a79408d217847fcb84fd069560
Johan Kämpe <kampe.johan@gmail.com>
2017-09-06 14:55:49 +0200

    voteMachine init

---
+++ voteMachine_App/voteApp/Main.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace voteApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM6";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(42, 133);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(504, 31);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(580, 133);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(212, 65);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Skicka!";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1241, 809);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "frmMain";
+            this.Text = "voteMachine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ voteMachine_App/voteApp/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace voteApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            OpenCOM();
+        }
+
+        void OpenCOM()
+        {
+            if (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.Open();
+                }
+
+                catch (Exception)
+                {
+                    MessageBox.Show("Kan inte öppna " + serialPort.PortName);
+                }
+
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            serialPort.Write(textBoxInput.Text + '\0');
+        }
+    }
+
+
+}
